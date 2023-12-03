667fc7e41319baa244e112342038de96e8b75829
devfabien <ish001fabi@gmail.com>
2023-12-03 14:17:36 +0200

    adding a not before the matcher

--- function.test.js
+++ function.test.js
@@ -3,3 +3,7 @@ const functions = require('./functions')
 test("Adds 2 + 2 to equal 4",() => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Adds 2 + 2 to equal not to be 5",() => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
